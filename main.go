8bb5505a0319e4088dc03002fca8a442dd9d71b6
Yoshiki Fujikane <ffjlabo@gmail.com>
2021-04-20 23:34:24 +0900

    fix: 関数名を変更してコードを保存する処理を追加

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"go/ast"
 	"go/parser"
+	"go/printer"
 	"go/token"
+	"log"
+	"os"
 )
 
 func main() {
@@ -13,10 +15,21 @@ func main() {
 
 	ast.Inspect(f, func(n ast.Node) bool {
 		if v, ok := n.(*ast.FuncDecl); ok {
-			fmt.Println(v.Name)
-			fmt.Println(fset.Position(v.Pos()))
+			// ノードを書き換える
+			v.Name = &ast.Ident{
+				Name: "plus",
+			}
 		}
 
 		return true
 	})
+
+	file, err := os.OpenFile("example/result.go", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	pp := &printer.Config{Tabwidth: 8, Mode: printer.UseSpaces | printer.TabIndent}
+	pp.Fprint(file, fset, f)
 }
