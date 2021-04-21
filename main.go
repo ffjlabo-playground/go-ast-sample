094b90a645f4f913edcb4f76a04883de4dcdf044
Yoshiki Fujikane <ffjlabo@gmail.com>
2021-04-22 03:09:10 +0900

    fix: 構造体名と構造体の要素名、要素名の型名を取得するように修正

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 	fset := token.NewFileSet()
-	f, _ := parser.ParseFile(fset, "./example/example.go", nil, parser.Mode(0))
+	f, _ := parser.ParseFile(fset, "./example/helloworld.pb.go", nil, parser.Mode(0))
+
+	structASTMap := map[string]*ast.StructType{}
 
 	for _, decl := range f.Decls {
 		// struct型の型定義を表示する
@@ -32,8 +34,37 @@ func main() {
 			if !ok {
 				continue
 			}
-			fmt.Println(s.Name)
-			ast.Print(fset, t)
+
+			structASTMap[s.Name.Name] = t
+		}
+	}
+
+	// structの各要素を取得する
+	for name, node := range structASTMap {
+		// struct名
+		fmt.Println(name)
+		for _, field := range node.Fields.List {
+
+			for _, nameIdent := range field.Names {
+				// 要素名
+				fmt.Print("    " + nameIdent.Name + " ")
+
+				// 要素の型名
+				switch field.Type.(type) {
+				// 別パッケージの型を利用している場合
+				case *ast.SelectorExpr:
+					selectorExpr, _ := field.Type.(*ast.SelectorExpr)
+					xIdent, _ := selectorExpr.X.(*ast.Ident)
+					fmt.Println(xIdent.Name + "." + selectorExpr.Sel.Name)
+				// 組み込みまたはどうパッケージ内の型を利用している場合
+				case *ast.Ident:
+					ident, _ := field.Type.(*ast.Ident)
+					fmt.Println(ident.Name)
+				}
+				// typeSelectorExpr, _ := field.Type.(*ast.SelectorExpr)
+				// typeIdent, _ := typeSelectorExpr.X.(*ast.Ident)
+				// fmt.Println(field.Type)
+			}
 		}
 	}
 }
